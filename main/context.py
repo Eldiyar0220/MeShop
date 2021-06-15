881ec2f4ec7d7256eabcdd1a9049b11310eae333
Manap <ase21112013@gmail.com>
2021-06-15 15:31:41 +0600

    models

--- main/context.py
+++ main/context.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+
 from .models import Category
 
 def get_categories(request):
--- order/models.py
+++ order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from main.models import Products
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='orders')
+    phone = models.CharField(max_length=13)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.email} - {self.phone}'
